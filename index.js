8cab4ed2f29b16582f340d4a27575c4658c05fc2
Juliaaa25 <yulia.kaot@mail.ru>
2025-07-14 17:02:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const jsonURL = "https://juliaaa25.github.io/my-favorite-book/";
-
-fetch(jsonURL)
+fetch("https://juliaaa25.github.io/my-favorite-book/book.json")
   .then((response) => {
     if (!response.ok) {
-      throw new Error("Ошибка при загрузке JSON");
+      throw new Error("Не удалось загрузить JSON");
     }
     return response.json();
   })
   .then((data) => {
-    const bookDiv = document.getElementById("book-info");
-    bookDiv.innerHTML = `
+    const div = document.getElementById("book-info");
+    div.innerHTML = `
       <p><strong>Название:</strong> ${data.title}</p>
       <p><strong>Автор:</strong> ${data.author}</p>
       <p><strong>Год:</strong> ${data.year}</p>
     `;
   })
   .catch((error) => {
-    document.getElementById("book-info").textContent =
-      "Не удалось загрузить данные 😢";
+    document.getElementById("book-info").textContent = "Ошибка загрузки 😢";
     console.error(error);
   });
